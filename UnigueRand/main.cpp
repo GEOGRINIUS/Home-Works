a00e260e129ef3f261df418fbb62aa229beaf4c4
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2025-06-05 14:42:31 +0400

    update

---
+++ UnigueRand/main.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int SIZE = 5;
+	int arr[SIZE] = { 3, 5, 8 };
+	int minRand, maxRand;
+	cout << "Введите минимальное случайное число: "; cin >> minRand;
+	cout << "Введите максимальное случайное число: "; cin >> maxRand;
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	//Вывод массива на экран:
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
