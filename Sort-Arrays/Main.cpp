122450a29e605c1c9c24b2c256400ee4fb9123e1
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2025-06-07 15:55:56 +0400

    Update: Arrays()

---
+++ Sort-Arrays/Main.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+#include <algorithm> //Для sort
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int SIZE = 10;
+	int arr[SIZE] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int minRand, maxRand;
+	cout << "Введите минимальное случайное число: "; cin >> minRand;
+	cout << "Введите максимальное случайное число: "; cin >> maxRand;
+	srand(time(0)); //Иницализируется генератор чисел (sort как при помощи srand, как раз и сортирует числа.)
+	for (int i = 0; i < SIZE; i++)
+	{
+		arr[i] = rand() % (maxRand - minRand) + minRand;
+	}
+	sort(arr, arr + SIZE);  //sort отвечает сортировку чисел от меньшкего к большому
+	for (int i = 0; i < SIZE; i++)
+	{
+		cout << arr[i] << "\t";
+	}
+	cout << endl;
+}
--- UnigueRand/main.cpp
+++ UnigueRand/main.cpp
@@ -4,7 +4,7 @@ void main()
 {
 	setlocale(LC_ALL, "");
 	const int SIZE = 10;
-	int arr[SIZE] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	int arr[SIZE] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
 	int minRand, maxRand;
 	cout << "Введите минимальное случайное число: "; cin >> minRand;
 	cout << "Введите максимальное случайное число: "; cin >> maxRand;
