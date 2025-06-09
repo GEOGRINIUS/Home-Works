97074b9834dbcd655be5c2e46b7a7a210f36bec1
Gameboy <Gameboy@DESKTOP-M5JQ5GR>
2025-06-09 14:10:53 +0400

    Update: Arrays(mission: 1)

--- Shifts(left)/main.cpp
+++ Shifts(left)/main.cpp
@@ -1,17 +1,33 @@
-#include<iostream>
+#include <Windows.h>
+#include <iostream>
 using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int SIZE = 10;
-	int arr[SIZE] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-	for (int i = 0; i < SIZE; i++)
+	const int n = 10;
+	int arr[n] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
 	cout << endl;
-	for (int i = SIZE - 1; i >= 0; i--)
+	int number_of_shifts;
+	cout << "Введите количество сдвигов: "; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
 	{
-		cout << arr[i] << "\t";
+		//1) Сохраняем нулевой элемент массива
+		int buffer = arr[0];
+		//2) Сдвигаем массив:
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		//3) Записываем нулевой элемент в конец массива:
+		arr[n - 1] = buffer;
+		for (int i = 0; i < n; i++)
+		{
+			cout << arr[i] << "\t";
+		}
+		cout << endl;
 	}
 }
--- UnigueRand/main.cpp
+++ UnigueRand/main.cpp
@@ -3,19 +3,50 @@ using namespace std;
 void main()
 {
 	setlocale(LC_ALL, "");
-	const int SIZE = 10;
-	int arr[SIZE] = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
-	int minRand, maxRand;
-	cout << "Введите минимальное случайное число: "; cin >> minRand;
-	cout << "Введите максимальное случайное число: "; cin >> maxRand;
-	for (int i = 0; i < SIZE; i++)
+	const int n = 10;
+	int arr[n];
+	for (int i = 0; i < n; i++)
 	{
-		arr[i] = rand() % (maxRand - minRand) + minRand;
+		bool unigue;
+		do
+		{
+			arr[i] = rand() % 30;
+			unigue = true;
+			for (int j = 0; j < i; j++)
+			{
+				if (arr[i] == arr[j])
+				{
+					unigue = false;
+					break;
+				}
+			}
+		} while (!unigue);
 	}
-	//Вывод массива на экран:
-	for (int i = 0; i < SIZE; i++)
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+	for (int i = 0; i < n; i++)
 	{
 		cout << arr[i] << "\t";
 	}
-	cout << endl; 
+
+	cout << endl;
+	int min, max;
+	min = max = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		if (arr[i] > max)max = arr[i];
+	}
+	cout << "Минимальное значение в массиве: " << min << endl;
+	cout << "Максимальное значение в массиве: " << max << endl;
 }
